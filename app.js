2127072255858e3eef2cc286cfd4ca9e921f2a0c
juan agustin tibaldo <juanagustintibaldo@gmail.com>
2018-08-08 22:00:58 -0300

    cleaning up

--- app.js
+++ app.js
@@ -1,138 +1,119 @@
-var THREE = require("three");
-var fs = require("fs");
-var savePixels = require("save-pixels")
-var ndarray = require("ndarray")
+const fs = require("fs");
+const Canvas = require("canvas");
+const glContext = require('gl')(1,1); //headless-gl
 
-// Create a DOM
-var MockBrowser = require('mock-browser').mocks.MockBrowser;
-var mock = new MockBrowser();
-var document = MockBrowser.createDocument();
-//REST API
-var express     = require('express');      
-var app         = express();    
-var bodyParser  = require('body-parser');
-var router = express.Router();
-global.navigator = require('web-midi-api');
+const THREE = require("three");
 global.THREE = THREE;
 
-	
-var width   = 128
-var height  = 128
+//a navigator to be used internally
+const window = {innerWidth: 1024, innerHeight: 1024};
+global.navigator = require('web-midi-api');
+// out tool to export the scene into an image
+const pngStream = require('three-png-stream');
 
-var pngStream = require('three-png-stream');
-var port = process.env.PORT || 8080;
-var Canvas = require("canvas");
+let camera, scene, renderer, teximage, canvasGL, target, directional_light, ambient_light, cube, texture_cube;
 
-var window = {innerWidth: width, innerHeight: height};
+function init() {
 
-THREE.document = document;
-window.document = document;
-global.document = document;
-global.window = window;
-app.get('/render', function(req, res){
-
+  // GL target
+  target = new THREE.WebGLRenderTarget(window.innerWidth, window.innerHeight);
   canvasGL = new Canvas(window.innerWidth, window.innerHeight);
-  canvasGL.addEventListener = function(event, func, bind_) {}; // mock function to avoid errors inside THREE.WebGlRenderer()
 
-	var gl = require('gl')(width, height, { preserveDrawingBuffer: true, canvas: canvasGL })
-	var document = MockBrowser.createDocument();
+  // mock function to avoid errors inside THREE.WebGlRenderer()
+  canvasGL.addEventListener = function(event, func, bind_) {};
 
-    var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera(75, this.width / this.height, 0.1, 1000);
-    var renderer = new THREE.WebGLRenderer({context:gl});
-
-    renderer.setSize(this.width, this.height);
-    renderer.setClearColor(0xFFFF00, 1);
+  renderer = new THREE.WebGLRenderer( { preserveDrawingBuffer: true, context: glContext, antialias: true, canvas: canvasGL });
+  renderer.setClearColor(0xff0000, 1);
+  //
   renderer.autoClear = false;
   renderer.clear()
 
-    camera.position.z = 10;
-    scene.add(camera);
+  // near can't be less than 1
+  camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 100000 );
+  camera.position.set( 10, 12, 20 );
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
 
+  //////////////////////////////////////////////////////////////////////
+  scene = new THREE.Scene();
+  // lights
+  directional_light = new THREE.DirectionalLight(0xffffff, 1.2);
+  directional_light.position.set(10,10,10);
+  scene.add(directional_light);
 
-  //var geometry = new THREE.BoxGeometry( 10, 10, 10, 1,1,1 );
-  //var material = new THREE.MeshBasicMaterial( {color: 0x123456, side: THREE.DoubleSide} );
-  //var plane = new THREE.Mesh( geometry, material );
-  //scene.add(plane)
-  //camera.lookAt(plane)
+  ambient_light = new THREE.AmbientLight(0xffffff, 0.2);
+  scene.add(ambient_light);
 
+  // untextured purple plane
+  let geometry = new THREE.BoxGeometry( 10, 10, 10);
+  let material = new THREE.MeshPhongMaterial( {color: 0x0000ff, side: THREE.DoubleSide} );
+  cube = new THREE.Mesh( geometry, material );
+  cube.position.z = -20
+  cube.rotation.y = Math.PI * 0.25
+  scene.add( cube );
 
-    var target = new THREE.WebGLRenderTarget(this.width, this.height);
+  material = new THREE.MeshPhongMaterial({color : 0xffffff, side: THREE.DoubleSide});
+  var texture_cube = new THREE.Mesh( geometry, material );
+  texture_cube.position.y = 10;
+  texture_cube.position.z = -20;
+  texture_cube.rotation.y = Math.PI * 0.25;
+  texture_cube.scale.set(0.7, -0.7, 0.7);
 
-   renderer.render(scene, camera);
-   //res.setHeader('Content-Type', 'image/png');
-   //pngStream(renderer, target).pipe(res);
+  scene.add( texture_cube );
 
-	var pixels = new Uint8Array(width * height * 4)
+  // TEXTURE LOADERS ////////////////////////////////////////////////////////////////////////
+  var getPixels = require("get-pixels")
 
-   gl.readPixels(0, 0, width, height, gl.RGBA, gl.UNSIGNED_BYTE, pixels);
+  getPixels(__dirname + '/texture.jpg', function(err, pixels) {
 
-   	//console.log(renderer.domElement)
-  	setTimeout( function() {
+      if(err) {
+        console.log("Failed to load texture using get-pixels:", err);
+        return;
+      }
 
+      var texture = new THREE.DataTexture( new Uint8Array(pixels.data), pixels.shape[0], pixels.shape[1], THREE.RGBAFormat);
+      texture.needsUpdate = true;
 
-var dataURL = canvasGL.toDataURL("image/png");//canvasGL.toDataURL();
+      texture_cube.material.map = texture;
 
-									//data:image/png;base64,
-  	var dataURL = dataURL.replace(/^data:image\/png;base64,/, "");
+  });
 
-   	 fs.writeFile('./asd3.png', dataURL, function(err) {
-   	console.log(dataURL)
-
-    if(err) {
-        return console.log(err);
-    }
-    }); 
-
-	process.stdout.write(['P3\n# gl.ppm\n', width, " ", height, '\n255\n'].join(''))
-	for(var i=0; i<pixels.length; i+=4) {
-	  for(var j=0; j<3; ++j) {
-
-	    //process.stdout.write(pixels[i+j] + ' ')
-	  }
-	}
-	//ndarray(gl.RGBA, [window.innerHeight, window.innerWidth]);
-
-	//savePixels([pixels], "png").pipe(process.stdout)
-
-    var img = new Buffer(dataURL, 'base64');
-    console.log(img)
-
-
-   res.writeHead(200, {
-     'Content-Type': 'image/png',
-     'Content-Length': img.length
-   });
-   res.end(img);
-
-
-  	}, 1000)
-  	
-
-   });
-/*
-app.get('/render', function(req, res){
-	
-var width   = 64
-var height  = 64
-var gl = require('gl')(width, height, { preserveDrawingBuffer: true })
-
-//Clear screen to red
-gl.clearColor(1, 0, 0, 1)
-gl.clear(gl.COLOR_BUFFER_BIT)
-
-//Write output as a PPM formatted image
-var pixels = new Uint8Array(width * height * 4)
-gl.readPixels(0, 0, width, height, gl.RGBA, gl.UNSIGNED_BYTE, pixels)
-process.stdout.write(['P3\n# gl.ppm\n', width, " ", height, '\n255\n'].join(''))
-for(var i=0; i<pixels.length; i+=4) {
-  for(var j=0; j<3; ++j) {
-    process.stdout.write(pixels[i+j] + ' ')
-  }
 }
-});
-*/
-//app.use('/api', router);
 
-app.listen(port);
-console.log('Server active on port: ' + port);
+function render() {
+   renderer.render( scene, camera, target );
+}
+
+//
+function exportImage(exportPath) {
+
+  console.log("The file was saved!");
+
+  var output = fs.createWriteStream(exportPath)
+
+  pngStream(renderer, target)
+    .pipe(output);
+}
+
+/// textures needs to be called, that's why we need a delay for now
+function renderAndExport(exportPath, delay = 0) {
+
+  const render_call = () => {
+    console.log('rendering...');
+    render();
+    console.log('exporting...');
+    exportImage(exportPath);
+    console.log('done');
+  };
+
+  if ( delay > 0) {
+    console.log('waiting '+delay+' ms in case texture initialization takes time...');
+  }
+
+  setTimeout(function(){ render_call(); }, delay);
+
+}
+
+init();
+
+renderAndExport("./image2.png", 100);
