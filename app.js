c74f9cc7be5e61b93e17c3efee6af94c29778d06
juan agustin tibaldo <juanagustintibaldo@gmail.com>
2018-08-09 18:51:18 -0300

    a few tweaks

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const Canvas = require("canvas");
 const glContext = require('gl')(1,1); //headless-gl
+const getPixels = require("get-pixels");
 
 const THREE = require("three");
 global.THREE = THREE;
@@ -37,47 +38,6 @@ function init() {
   //////////////////////////////////////////////////////////////////////
   scene = new THREE.Scene();
   // lights
-  directional_light = new THREE.DirectionalLight(0xffffff, 1.2);
-  directional_light.position.set(10,10,10);
-  scene.add(directional_light);
-
-  ambient_light = new THREE.AmbientLight(0xffffff, 0.2);
-  scene.add(ambient_light);
-
-  // untextured purple plane
-  let geometry = new THREE.BoxGeometry( 10, 10, 10);
-  let material = new THREE.MeshPhongMaterial( {color: 0x0000ff, side: THREE.DoubleSide} );
-  cube = new THREE.Mesh( geometry, material );
-  cube.position.z = -20
-  cube.rotation.y = Math.PI * 0.25
-  scene.add( cube );
-
-  material = new THREE.MeshPhongMaterial({color : 0xffffff, side: THREE.DoubleSide});
-  var texture_cube = new THREE.Mesh( geometry, material );
-  texture_cube.position.y = 10;
-  texture_cube.position.z = -20;
-  texture_cube.rotation.y = Math.PI * 0.25;
-  texture_cube.scale.set(0.7, -0.7, 0.7);
-
-  scene.add( texture_cube );
-
-  // TEXTURE LOADERS ////////////////////////////////////////////////////////////////////////
-  var getPixels = require("get-pixels")
-
-  getPixels(__dirname + '/texture.jpg', function(err, pixels) {
-
-      if(err) {
-        console.log("Failed to load texture using get-pixels:", err);
-        return;
-      }
-
-      var texture = new THREE.DataTexture( new Uint8Array(pixels.data), pixels.shape[0], pixels.shape[1], THREE.RGBAFormat);
-      texture.needsUpdate = true;
-
-      texture_cube.material.map = texture;
-
-  });
-
 }
 
 function render() {
@@ -116,4 +76,58 @@ function renderAndExport(exportPath, delay = 0) {
 
 init();
 
-renderAndExport("./image2.png", 100);
+
+const load_texture = (texture_path, property, where) => {
+  getPixels(__dirname + texture_path, function(err, pixels) {
+
+      if(err) {
+        console.log("Failed to load texture using get-pixels:", err);
+        return;
+      }
+
+      var texture = new THREE.DataTexture( new Uint8Array(pixels.data), pixels.shape[0], pixels.shape[1], THREE.RGBAFormat);
+      texture.needsUpdate = true;
+
+      property[where] = texture;
+
+  });
+}
+
+const objects_to_display = () => {
+
+  const obj = new THREE.Object3D();
+
+  directional_light = new THREE.DirectionalLight(0xffffff, 1.2);
+  directional_light.position.set(10,10,10);
+  obj.add(directional_light);
+
+  ambient_light = new THREE.AmbientLight(0xffffff, 0.2);
+  obj.add(ambient_light);
+  // untextured purple plane
+  let geometry = new THREE.BoxGeometry( 10, 10, 10);
+  let material = new THREE.MeshPhongMaterial({ color: 0x0000ff, side: THREE.DoubleSide });
+  cube = new THREE.Mesh( geometry, material );
+  cube.position.z = -20
+  cube.rotation.y = Math.PI * 0.25
+  obj.add( cube );
+
+  material = new THREE.MeshPhongMaterial({ color : 0xffffff, side: THREE.DoubleSide });
+  var texture_cube = new THREE.Mesh( geometry, material );
+  texture_cube.position.y = 10;
+  texture_cube.position.z = -20;
+  texture_cube.rotation.y = Math.PI * 0.25;
+  texture_cube.scale.set(0.7, -0.7, 0.7);
+
+  obj.add( texture_cube );
+  load_texture('/texture.jpg', texture_cube.material, 'map');
+  // TEXTURE LOADERS ////////////////////////////////////////////////////////////////////////
+  return obj;
+
+}
+
+scene.add(objects_to_display());
+const obj2 = objects_to_display();
+obj2.position.x += 20;
+scene.add(obj2);
+
+renderAndExport("./image2.png", 200);
